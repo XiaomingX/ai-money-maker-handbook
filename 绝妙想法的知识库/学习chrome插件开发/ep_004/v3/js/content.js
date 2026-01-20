7d45f9fff50180880a5f71eec577aa0be5101160
xiaoming <lininruc@gmail.com>
2026-01-20 08:58:11 +0800

    mm

--- 绝妙想法的知识库/学习chrome插件开发/ep_004/v3/js/content.js
+++
@@ -1,7 +0,0 @@
-// let us know we're running
-console.log("Content script has loaded via Manifest V3.");
-
-// send a message to the background requesting an additional script
-chrome.runtime.sendMessage({
-  cmd: "runLogic"
-});
--- 绝妙想法的知识库/学习chrome插件开发/ep_006/v3/js/content.js
+++
@@ -1,9 +0,0 @@
-// send a message to the background requesting our business logic
-chrome.runtime.sendMessage({
-  cmd: "runLogic"
-});
-
-// send a message to the background requesting that a context menu be attached
-chrome.runtime.sendMessage({
-  cmd: "addMenu"
-});
